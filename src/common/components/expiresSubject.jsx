e0da877d148296a6b96587f89b3aa6db75c787b6
June <nhoxtuanhero1@gmail.com>
2023-09-01 11:24:30 +0700

    fix bug

--- src/common/components/expiresSubject.jsx
+++ src/common/components/expiresSubject.jsx
@@ -5,21 +5,21 @@ import { useSelector } from "react-redux";
 
 const ExpiresSubject = () => {
   const { today } = useSelector((state) => state.schedule);
-  const checkMorning = today.morning.filter(
+  const checkMorning = today.morning?.filter(
     (schedule) => schedule.state !== ""
   );
-  const checkAfternoon = today.afternoon.filter(
+  const checkAfternoon = today.afternoon?.filter(
     (schedule) => schedule.state !== ""
   );
-  const checkEvenning = today.evenning.filter(
+  const checkEvenning = today.evenning?.filter(
     (schedule) => schedule.state !== ""
   );
 
-  const final = checkMorning.concat(checkAfternoon).concat(checkEvenning);
+  const final = checkMorning?.concat(checkAfternoon)?.concat(checkEvenning);
 
-  const succes = final.filter((item) => item.state === "success");
-  const warning = final.filter((item) => item.state === "warning");
-  const missing = final.filter((item) => item.state === "missing");
+  const succes = final?.filter((item) => item.state === "success");
+  const warning = final?.filter((item) => item.state === "warning");
+  const missing = final?.filter((item) => item.state === "missing");
 
   const [data, setData] = useState({
     success: null,
@@ -42,19 +42,19 @@ const ExpiresSubject = () => {
       <div className="expires_sub">
         <div className="expires_sub_success">
           <span></span>
-          <p>{data.success}(%)</p>
+          <p>{data?.success}(%)</p>
         </div>
         <div className="expires_sub_warning">
           <span></span>
-          <p>{data.warning}(%)</p>
+          <p>{data?.warning}(%)</p>
         </div>
         <div className="expires_sub_missing">
           <span></span>
-          <p>{data.missing}(%)</p>
+          <p>{data?.missing}(%)</p>
         </div>
       </div>
       <div className="expires_subject_list">
-        {final.map((schedule, i) => (
+        {final?.map((schedule, i) => (
           <Subject type="expires" key={i} data={{ ...schedule, i }} />
         ))}
       </div>
